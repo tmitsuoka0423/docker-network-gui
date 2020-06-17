04f16dffd2a6e8e6baf7e35f1c91a7b6a830fc9b
mitsuoka-takahiro <mitsuoka-takahiro@dmm.com>
2020-06-17 21:56:35 +0900

    update

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
-export default function Home() {
+export default function Home({ dockerNetworkList }: any) {
+  const linkList = dockerNetworkList.map(dockerNetwork => (
+    <div key={dockerNetwork.name}>
+      <Link to={routes.DETAIL}>{dockerNetwork.name}</Link>
+    </div>
+  ));
+
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <div>$ docker network ls</div>
+      {linkList}
     </div>
   );
 }
--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,6 +1,48 @@
 import React from 'react';
 import Home from '../components/Home';
 
-export default function HomePage() {
-  return <Home />;
+const { exec } = require('child_process');
+
+export default class HomePage extends React.Component {
+  static async listDockerNetwork(): Promise<Array<object>> {
+    return new Promise((resolve, reject) => {
+      exec('docker network ls', (err, stdout) => {
+        let result: Array<object> = [];
+
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        result = stdout
+          .replace(/.*\n/, '')
+          .replace(/ +/g, ' ')
+          .split('\n')
+          .map(row => {
+            const cols = row.split(' ');
+
+            return {
+              networkId: cols[0],
+              name: cols[1],
+              driver: cols[2],
+              scope: cols[3]
+            };
+          });
+        resolve(result);
+      });
+    });
+  }
+
+  private dockerNetworkList: Array<object> = [];
+
+  async componentDidMount() {
+    console.log('start componentDidMount');
+    this.dockerNetworkList = await HomePage.listDockerNetwork();
+    console.log(this.dockerNetworkList);
+    console.log('end componentDidMount');
+  }
+
+  render() {
+    return <Home dockerNetworkList={this.dockerNetworkList} />;
+  }
 }
