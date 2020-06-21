d145888e1b6ca12e3fb6c4dc6c6fc2415013b810
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-06-21 10:04:46 +0900

    [WIP]add detail

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
-import Detail from './organizations/Detail';
-
 export default function Home({ dockerNetworkList }: any) {
   const linkList = dockerNetworkList.map(dockerNetwork => (
     <div key={dockerNetwork.name}>
@@ -16,7 +14,6 @@ export default function Home({ dockerNetworkList }: any) {
     <div className={styles.container} data-tid="container">
       <div>$ docker network ls</div>
       {linkList}
-      <Detail />
     </div>
   );
 }
