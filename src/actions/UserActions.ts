55e01e31603e806de71805fa5e7e3cb5bae63051
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-06-26 21:11:22 +0900

    remove file

--- src/actions/UserActions.ts
+++
@@ -1,6 +0,0 @@
-import { actionCreatorFactory } from 'typescript-fsa';
-import IUser from '../states/IUser';
-
-const actionCreator = actionCreatorFactory('user-action');
-
-export const changeUserAction = actionCreator<Partial<IUser>>('change-user');
