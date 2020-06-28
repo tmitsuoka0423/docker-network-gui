4555975c5f2a271be680be176d2cd2d9a6f097d1
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-06-28 16:50:20 +0900

    add doceker network ls

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,24 +1,52 @@
 import React, { useState, useEffect } from 'react';
+import { exec } from 'child_process';
 import styles from './styles.scss';
-import { ipcRenderer } from 'electron';
 import Button from '../atoms/Button/Button';
 
 const Main: React.FC = () => {
+  let list;
+
   const [dockerNetworkList, setDockerNetworkList] = useState([
-    { id: 'aaa', name: 'aaaa' },
-    { id: 'bbb', name: 'bbbb' },
-    { id: 'ccc', name: 'cccc' },
+    {
+      id: '',
+      name: '',
+      driver: '',
+      scope: '',
+    },
   ]);
 
   useEffect(() => {
-    ipcRenderer.invoke('get-docker-network-list').then(dockerNetworkList => {
+    exec('docker network ls', (err, stdout) => {
+      if (err) throw err;
+
+      const dockerNetworkList = stdout
+        .replace(/ +/g, ' ')
+        .replace(/\n$/, '')
+        .split('\n')
+        .filter(row => {
+          return !row.includes('NETWORK ID');
+        })
+        .map(row => {
+          console.log(row);
+          const cols = row.split(' ');
+          return {
+            id: cols[0],
+            name: cols[1],
+            driver: cols[2],
+            scope: cols[3],
+          };
+        });
       setDockerNetworkList(dockerNetworkList);
     });
   }, []);
 
-  const list = dockerNetworkList.map(dockerNetwork => {
-    return <Button label={dockerNetwork.name} key={dockerNetwork.id}></Button>;
-  });
+  if (dockerNetworkList.length === 0) {
+    list = <div>no docker network found</div>;
+  } else {
+    list = dockerNetworkList.map(dockerNetwork => {
+      return <Button label={dockerNetwork.name} key={dockerNetwork.id}></Button>;
+    });
+  }
 
   return <div className={styles.main}>{list}</div>;
 };
